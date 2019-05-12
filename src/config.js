657733032f47499aac6e6fe4aed929fbc1d341b5
Radoslav Stankov <rstankov@gmail.com>
2019-05-12 18:40:40 +0300

    Update for 2019

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ import React from 'react';
 import wifiImage from './App/wifi.svg';
 
 export default {
-  // time: new Date('2019-05-11T09:45:00.000+00:00'),
+  time: new Date('2019-05-11T15:30:00.000+00:00'),
   fetchUrl: 'http://react-not-a-conf.com/schedules/2019.json',
   sponsorLogoHeight: 40,
   footerText: (
     <span>
-      <img src={wifiImage} alt="" height="16" /> generator / NoPassword
+      <img src={wifiImage} alt="" height="16" /> Sofia hall / Sofiahall2018
     </span>
   ),
 };
